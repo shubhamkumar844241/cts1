941e891d66473db1fbeef82605e4e211c7d6b9ee
shubhamkumar844241 <60208801+shubhamkumar844241@users.noreply.github.com>
2020-01-25 03:56:21 -0800

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add{
+
+public static void main(String arg[])
+{
+int x,y,sum;
+System.out.println("numbers are");
+System.out.println("first number" +Integer.parseInt(arg[0]));
+System.out.println("second nmbr" +Integer.parseInt(arg[1]));
+x=Integer.parseInt(arg[0]);
+y=Integer.parseInt(arg[1]);
+sum=x+y;
+System.out.println("sum is " +sum);
+}
+}
